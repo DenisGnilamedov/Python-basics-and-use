1b7b8f04f378fa6e516ac289f97c531b3ed54d85
DenisGnilamedov <77381857+DenisGnilamedov@users.noreply.github.com>
2021-08-22 18:56:45 +0300

    2.1 exc inher - разобраться

--- 2.1 exception inheritance.py
+++ 2.1 exception inheritance.py
@@ -1,1 +1,12 @@
 n = int(input())
+d = {}
+for i in range(n):
+    child, *parent = input().replace(":", " ").split()
+    if child not in d:
+        d.update({child: parent})
+    else:
+        d[child] += parent
+
+q = int(input())
+for i in range(q):
+    errname = int(input())
