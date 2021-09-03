f75603018e38361efd97e2d9b2c31e6361543dca
DenisGnilamedov <77381857+DenisGnilamedov@users.noreply.github.com>
2021-09-03 22:58:01 +0300

    2.3 solved

--- 2.3 iter gen multifilter.py
+++ 2.3 iter gen multifilter.py
@@ -1,17 +1,45 @@
 class multifilter:
     def judge_half(pos, neg):
-        # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
+        return pos >= neg
 
     def judge_any(pos, neg):
-        # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
+        return pos >= 1
 
     def judge_all(pos, neg):
-        # допускает элемент, если его допускают все функции (neg == 0)
+        return neg == 0
 
     def __init__(self, iterable, *funcs, judge=judge_any):
-        # iterable - исходная последовательность
-        # funcs - допускающие функции
-        # judge - решающая функция
+        self.iterable = iterable    # iterable - исходная последовательность
+        self.funcs = funcs  # funcs - допускающие функции
+        self.judge = judge  # judge - решающая функция
 
     def __iter__(self):
-        # возвращает итератор по результирующей последовательности
+        for i in self.iterable:
+            pos = 0
+            neg = 0
+            for func in self.funcs: # пробегаем по функциям (mul2,3,5), если истина - pos+=1
+                if func(i):
+                    pos += 1
+                else:
+                    neg += 1
+            if self.judge(pos, neg): # сравниваем pos и neg в каждой итерации, если удовлетворяет условиям judge, добавляем элемент в print.
+                yield i
+
+
+def mul2(x):
+    return x % 2 == 0
+
+def mul3(x):
+    return x % 3 == 0
+
+def mul5(x):
+    return x % 5 == 0
+
+
+a = [i for i in range(31)] # [0, 1, 2, ... , 30]
+print(list(multifilter(a, mul2, mul3, mul5)))
+
+
+
+
+
