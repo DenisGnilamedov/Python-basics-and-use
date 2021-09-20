422eb20b1505eb4f124a9edc0a3df5a41460deac
DenisGnilamedov <77381857+DenisGnilamedov@users.noreply.github.com>
2021-09-20 19:12:13 +0300

    2.4 solved

--- 2.4 work with os.py
+++ 2.4 work with os.py
@@ -3,12 +3,13 @@ import os
 import os.path
 import glob
 
+a = []
 with zipfile.ZipFile("C:/1/main.zip", 'r') as zip_file:
     zip_file.extractall("C:/1/")
-for dirpath, dirname, files in os.walk("C:/1/main"):
-    if glob.glob("*.py"):
-        answer = "\n".join(dirname)
+os.chdir("C:/1/")
+for dirpath, dirname, files in os.walk(r"."):
+    for file in files:
+        if file[-3:] == '.py':
+            print(dirpath[2:])
+            break
 
-
-'''with open("C:/1/2_4.txt", 'a') as ouf:
-    ouf.write(answer)'''
